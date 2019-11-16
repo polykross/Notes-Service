efeb83aa7393c8307630f1deb4f638d6ae3949cb
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-11-16 12:55:00 +0200

    Add db providers module
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ Notes/DBProviders/IDBProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Notes.DBModels;
+
+namespace Notes.DBProviders
+{
+    public interface IDBProvider : IDisposable
+    {
+        IEnumerable<TObject> SelectAll<TObject>(Func<TObject, bool> selectFunc = null) where TObject : class, IDBModel;
+        TObject Select<TObject>(Func<TObject, bool> selectFunc) where TObject : class, IDBModel;
+        void Update<TObject>(IEnumerable<TObject> objects) where TObject : class, IDBModel;
+        void Update<TObject>(TObject obj) where TObject : class, IDBModel;
+        void Delete<TObject>(IEnumerable<TObject> obj) where TObject : class, IDBModel;
+        void Delete<TObject>(TObject obj) where TObject : class, IDBModel;
+        void Add<TObject>(IEnumerable<TObject> obj) where TObject : class, IDBModel;
+        void Add<TObject>(TObject obj) where TObject : class, IDBModel;
+    }
+}
