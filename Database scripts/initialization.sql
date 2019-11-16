7543998474005b99d067568f6e4ebef492f05035
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-11-16 10:56:13 +0200

    Modify database initialization script
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- Database scripts/initialization.sql
+++ Database scripts/initialization.sql
@@ -6,31 +6,8 @@ To create database execute:
 create database Notes;
 */
 
-drop table if exists Page;
 drop table if exists Note;
 drop table if exists Customer;
-
-create table dbo.Page
-(
-    uuid      UNIQUEIDENTIFIER default NEWID(),
-    number    integer          not null,
-    text      text             not null,
-    note_uuid UNIQUEIDENTIFIER not null,
-    constraint page_pq primary key (uuid),
-    constraint page_note_fk foreign key (note_uuid) references Note (uuid)
-);
-
-create table dbo.Note
-(
-    uuid           UNIQUEIDENTIFIER default NEWID(),
-    title          varchar(255)     not null,
-    creation_date  datetime         not null,
-    last_edit_date datetime         not null,
-    customer_uuid  UNIQUEIDENTIFIER not null,
-    constraint note_pq primary key (uuid),
-    constraint note_customer_fk foreign key (customer_uuid) references Customer (uuid)
-
-);
 
 create table dbo.Customer
 (
@@ -39,4 +16,17 @@ create table dbo.Customer
     password varchar(255) not null,
     constraint customer_pk primary key (uuid),
     constraint customer_login_uq unique (login)
+);
+
+create table dbo.Note
+(
+    uuid           UNIQUEIDENTIFIER default NEWID(),
+    title          varchar(255)     not null,
+    text           varchar(max)     not null,
+    creation_date  datetime         not null,
+    last_edit_date datetime         not null,
+    customer_uuid  UNIQUEIDENTIFIER not null,
+    constraint note_pq primary key (uuid),
+    constraint note_customer_fk foreign key (customer_uuid) references Customer (uuid)
+
 );
