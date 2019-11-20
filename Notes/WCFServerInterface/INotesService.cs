7762b4902c1b38513de23c728daf6691bd250240
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-11-20 17:26:12 +0200

    Add WCFServiceInterface
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ Notes/WCFServerInterface/INotesService.cs
@@ -0,0 +1,16 @@
+﻿using Notes.DBModels;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace Notes.Server.WCFServerInterface
+{
+    [ServiceContract]
+    public interface INotesService
+    {
+        [OperationContract]
+        IEnumerable<Customer> GetAllCustomers();
+
+        [OperationContract]
+        void AddCustomer(Customer customer);
+    }
+}
