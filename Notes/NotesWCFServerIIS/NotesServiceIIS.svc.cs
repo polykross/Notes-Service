fcd7561ca7675b906297fdfe5d7dd6a57d3feba4
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-11-20 22:38:48 +0200

    Add NotesServiceIISl
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ Notes/NotesWCFServerIIS/NotesServiceIIS.svc.cs
@@ -0,0 +1,21 @@
+﻿using Notes.DTO;
+using Notes.Server.NotesServiceImplementation;
+using Notes.Server.WCFServerInterface;
+using System.Collections.Generic;
+
+namespace Notes.Server.WCFServerIIS
+{
+    public class NotesServiceIIS : INotesService
+    {
+        private readonly INotesService _service = new NotesService();
+        public IEnumerable<CustomerDTO> GetAllCustomers()
+        {
+            return _service.GetAllCustomers();
+        }
+
+        public void AddCustomer(CustomerDTO customer)
+        {
+            _service.AddCustomer(customer);
+        }
+    }
+}
