684bc801e0da1ee66f9f474fdc664391adf9ea6d
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-11-20 23:14:45 +0200

    Add NotesWCFService
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ Notes/WCFServer/NotesWCFService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+using Notes.Server.NotesServiceImplementation;
+
+namespace Notes.Server.WCFServer
+{
+    partial class NotesWCFService : ServiceBase
+    {
+        internal const string CurrentServiceName = "NotesService";
+        internal const string CurrentServiceDisplayName = "Notes Service";
+        internal const string CurrentServiceSource = "NotesServiceSource";
+        internal const string CurrentServiceLogName = "NotesServiceLogName";
+        internal const string CurrentServiceDescription = "Notes for learning purposes.";
+        private ServiceHost _serviceHost = null;
+
+        public NotesWCFService()
+        {
+            InitializeComponent();
+            ServiceName = CurrentServiceName;
+            AppDomain.CurrentDomain.UnhandledException += UnhandledException;
+        }
+
+        private void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            //TODO implement Logging
+        }
+
+        protected override void OnStart(string[] args)
+        {
+#if DEBUG
+            RequestAdditionalTime(120 * 1000);
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    Thread.Sleep(1000);
+            //}
+#endif
+            _serviceHost?.Close();
+            try
+            {
+                _serviceHost = new ServiceHost(typeof(NotesService));
+                _serviceHost.Open();
+            }
+            catch (Exception ex)
+            {
+                //TODO implement Logging
+                throw;
+            }
+        }
+
+        protected override void OnStop()
+        {
+            RequestAdditionalTime(120 * 1000);
+            try
+            {
+                _serviceHost.Close();
+            }
+            catch (Exception ex)
+            {
+                //TODO add Logging
+            }
+        }
+    }
+}
---
+++ Notes/WCFServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Server.WCFServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
