c88f6d620300ca3dde6d5c197572ead7ac34b441
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-11-23 18:31:05 +0200

    Create LoggerHelper
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- Notes/IntegrationTests/LoggerTests.cs
+++ Notes/IntegrationTests/LoggerTests.cs
@@ -5,6 +5,7 @@ using log4net.Config;
 using log4net.Core;
 using log4net.Layout;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Notes.Logger;
 
 namespace Notes.IntegrationTests
 {
@@ -12,57 +13,9 @@ namespace Notes.IntegrationTests
     public class LoggerTests
     {
         [TestMethod]
-        public void Log4NetConsoleAppenderTest()
-        {
-            var patternLayout = new PatternLayout
-            {
-                ConversionPattern = "%date{dd MMM yyyy HH:mm:ss} [%level] [%class] [%method] -> %message%newline"
-            };
-            patternLayout.ActivateOptions();
-
-            var consoleAppender = new ConsoleAppender()
-            {
-                Name = "ConsoleAppender",
-                Layout = patternLayout,
-                Threshold = Level.All
-            };
-
-            consoleAppender.ActivateOptions();
-            BasicConfigurator.Configure(consoleAppender);
-
-            var logger = LogManager.GetLogger(typeof(LoggerTests));
-
-            logger.Fatal("Fatal");
-            logger.Error("Error");
-            logger.Warn("Warn");
-            logger.Info("Info");
-            logger.Debug("Debug");
-        }
-    
-        [TestMethod]
         public void Log4NetFileAppenderTest()
         {
-            var patternLayout = new PatternLayout
-            {
-                ConversionPattern = "%date{dd MMM yyyy HH:mm:ss} [%level] [%class] [%method] -> %message%newline"
-            };
-            patternLayout.ActivateOptions();
-
-            var fileAppender = new RollingFileAppender()
-            {
-                Name = "FileAppender",
-                Layout = patternLayout,
-                Threshold = Level.All,
-                AppendToFile = true,
-                File = "NotesLog4Net.log",
-                MaximumFileSize = "10MB",
-                MaxSizeRollBackups = 100
-            };
-
-            fileAppender.ActivateOptions();
-            BasicConfigurator.Configure(fileAppender);
-
-            var logger = LogManager.GetLogger(typeof(LoggerTests));
+            var logger = LoggerHelper.GetLogger(typeof(LoggerTests));
 
             logger.Fatal("Fatal");
             logger.Error("Error");
---
+++ Notes/Logger/LoggerHelper.cs
@@ -0,0 +1,59 @@
+﻿using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Core;
+using log4net.Layout;
+using System;
+
+namespace Notes.Logger
+{
+    public class LoggerHelper
+    {
+        #region Fields
+        private static RollingFileAppender _rollingFileAppender;
+        private const string Layout = "%date{dd MMM yyyy HH:mm:ss} [%level] [%class] [%method] -> %message%newline";
+        #endregion
+
+        #region API
+        public static ILog GetLogger(Type type)
+        {
+            if (_rollingFileAppender == null)
+            {
+                _rollingFileAppender = GetFileAppender();
+            }
+
+            BasicConfigurator.Configure(_rollingFileAppender);
+
+            return LogManager.GetLogger(type);
+        }
+        #endregion
+
+        #region Util
+        private static PatternLayout GetPatternLayout()
+        {
+            var patternLayout = new PatternLayout
+            {
+                ConversionPattern = Layout
+            };
+            patternLayout.ActivateOptions();
+            return patternLayout;
+        }
+
+        private static RollingFileAppender GetFileAppender()
+        {
+            var fileAppender = new RollingFileAppender
+            {
+                Name = "FileAppender",
+                AppendToFile = true,
+                File = "NotesLog4Net.log",
+                Layout = GetPatternLayout(),
+                Threshold = Level.All,
+                MaximumFileSize = "10MB",
+                MaxSizeRollBackups = 100
+            };
+            fileAppender.ActivateOptions();
+            return fileAppender;
+        }
+        #endregion
+    }
+}
