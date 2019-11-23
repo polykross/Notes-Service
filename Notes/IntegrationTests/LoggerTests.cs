c4a4ee111ab578b043074044cc863cc07460c492
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-11-23 17:29:26 +0200

    Add Log4NetFileAppenderTest test
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- Notes/IntegrationTests/LoggerTests.cs
+++ Notes/IntegrationTests/LoggerTests.cs
@@ -1,4 +1,5 @@
-﻿using log4net;
+﻿using System;
+using log4net;
 using log4net.Appender;
 using log4net.Config;
 using log4net.Core;
@@ -37,6 +38,37 @@ namespace Notes.IntegrationTests
             logger.Info("Info");
             logger.Debug("Debug");
         }
+    
+        [TestMethod]
+        public void Log4NetFileAppenderTest()
+        {
+            var patternLayout = new PatternLayout
+            {
+                ConversionPattern = "%date{dd MMM yyyy HH:mm:ss} [%level] [%class] [%method] -> %message%newline"
+            };
+            patternLayout.ActivateOptions();
 
+            var fileAppender = new RollingFileAppender()
+            {
+                Name = "FileAppender",
+                Layout = patternLayout,
+                Threshold = Level.All,
+                AppendToFile = true,
+                File = "NotesLog4Net.log",
+                MaximumFileSize = "10MB",
+                MaxSizeRollBackups = 100
+            };
+
+            fileAppender.ActivateOptions();
+            BasicConfigurator.Configure(fileAppender);
+
+            var logger = LogManager.GetLogger(typeof(LoggerTests));
+
+            logger.Fatal("Fatal");
+            logger.Error("Error");
+            logger.Warn("Warn");
+            logger.Info("Info");
+            logger.Debug("Debug");
+        }
     }
 }
